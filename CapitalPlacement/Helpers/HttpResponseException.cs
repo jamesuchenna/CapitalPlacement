06105ba3bde367c79f120570ff49cf99579d0f34
jamesuchenna <jamescu@examroom.ai>
2024-06-20 13:41:28 +0100

    add HttpResponseException

---
+++ CapitalPlacement/Helpers/HttpResponseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CapitalPlacement;
+public class HttpResponseException : Exception
+{
+    public int StatusCode { get; }
+    public object? Value { get; }
+
+    public HttpResponseException(int statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+        Value = new { Message = message };
+    }
+}
+
