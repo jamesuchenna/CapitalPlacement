c1ef72005154295b27fd24bd8d5039e769f475c5
jamesuchenna <jamescu@examroom.ai>
2024-06-20 14:42:35 +0100

    add SubmitApplication endpoint

---
+++ CapitalPlacement/Controllers/ApplicationFormsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapitalPlacement.Controllers;
+
+[ApiController]
+[Route("[api/applicationForms]")]
+public class ApplicationFormController : ControllerBase
+{
+    private readonly IApplicationFormRepository _applicationRepository;
+
+    public ApplicationFormController(IApplicationFormRepository applicationRepository)
+    {
+        _applicationRepository = applicationRepository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SubmitApplication([FromBody] ApplicationFormDto dto)
+    {
+        var application = await _applicationRepository.CreateApplication(dto);
+        return Ok(application);
+    }
+}
+
